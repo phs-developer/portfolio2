cfd1bf1ebed6b230f1f44a2dbbb265817c445868
HeeSoon <phs970824@daum.net>
2023-06-23 01:57:44 +0900

    Header 영역 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  const Header = () => {
+    return (
+      <header className="flex">
+        <div className="container">
+          <h1>
+            반갑습니다,
+            <p>FE 개발자 입니다.</p>
+          </h1>
+          <h2>박희순</h2>
+          <ul className="flex">
+            <li>
+              <img src="./img/github.png" alt="github" />
+            </li>
+            <li>
+              <img src="./img/blog.png" alt="blog" />
+            </li>
+            <li>
+              <img src="./img/email.png" alt="email" />
+            </li>
+          </ul>
+        </div>
       </header>
+    );
+  };
+
+  return (
+    <div>
+      <Header />
     </div>
   );
 }
